c1e9ff0d60c028384bbe6bf4bedc1f5db72f7a25
Jason Ashton <ashtonguitars@gmail.com>
2017-11-02 04:58:35 +0000

    Converted to NVIDIA architecture

--- clone.py
+++ clone.py
@@ -29,7 +29,7 @@ def generator(samples, batch_size=32):
             for batch_sample in batch_samples:
                 for i in range(3):
                     current_path = './data/IMG/'+batch_sample[i].split('/')[-1]
-                    image = cv2.imread(current_path)
+                    image = cv2.imread(current_path) #this reads bgr, drive may send RGB
                     image = cv2.resize(image, (160, 80)) #size down
                     angle = float(batch_sample[3])
                     images.append(image)
@@ -60,21 +60,25 @@ model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(80,160,3)))
 model.add(Cropping2D(cropping=((25,10), (0,0)), input_shape=(3,80,160)))
 
-#lenet
-model.add(Convolution2D(6,5,5,activation="relu"))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6,5,5,activation="relu"))
-model.add(MaxPooling2D())
+#nvidia 
+model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+#model.add(Conv2D(64, 3, 3, padding="same", activation="relu"))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator, samples_per_epoch = \
                 len(train_samples), validation_data=validation_generator, \
-                nb_val_samples=len(validation_samples), nb_epoch=5)
+                nb_val_samples=len(validation_samples), nb_epoch=3)
 
 model.save('model.h5')
 exit()
+
+#todo: try dropout, verify the cropping of the image, make more test data
